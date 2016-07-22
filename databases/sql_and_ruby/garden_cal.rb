436760fbfd032e32cf7a684198f493025784aea8
Frank Aiello <frankvaiello@gmail.com>
2016-07-22 12:48:07 -0700

    finished view_log function

--- databases/sql_and_ruby/garden_cal.rb
+++ databases/sql_and_ruby/garden_cal.rb
@@ -19,12 +19,14 @@ create_table_cmd = <<-SQL
   INSERT INTO plants (name) VALUES ("Vegetables");
 SQL
 
+view_log_cmd = <<-SQL
+SELECT plants.name, water_log.water_date FROM
+   plants JOIN water_log
+   ON plants.id = water_log.plant_type
+SQL
+
 db.execute_batch(create_table_cmd)
 
-
-def split_date(date)
-  date.split(', ')
-end
 
 def insert_entry(db, date, plant)
   case plant
@@ -45,52 +47,36 @@ def make_entry(db)
     puts "year-month-day, (p = perennials, s = succulents, v = vegetables)"
     puts "ex: 2016-07-22, s    for succulents watered on July 22nd 2016"
     user_date = gets.chomp
-    water_date = split_date(user_date)[0]
-    plant_type = split_date(user_date)[1]
+    water_date = user_date.split(', ')[0]
+    plant_type = user_date.split(', ')[1]
     insert_entry(db, water_date, plant_type)
 end
 
-# def view_log(db)
-#   logs = db.execute("SELECT * FROM water_log")
-#   logs.each do |log|
-#     puts ""
-# end
+def view_log(db, view_log_cmd)
+  puts "-----------------------------------------"
+  logs = db.execute(view_log_cmd)
+  logs.each do |log|
+    puts "#{log[0]} were watered on #{log[1]}."
+  end
+end
 
 
 user_input = ""
-while user_input != "5"
-  puts "Type in the number for what you want to do with the garden log:"
+while user_input != "4"
+  puts "\nType in the number for what you want to do with the garden log:"
   puts "1. Make an entry"
   puts "2. View the garden log"
-  puts "3. Delete an entry"
-  puts "4. Get a reminder for the next time to water"
-  puts "5. Exit"
+  puts "3. Get a reminder for the next time to water"
+  puts "4. Exit"
   user_input = gets.chomp
 
   case user_input
     when "1"
       make_entry(db)
     when "2"
-      # view_log(db)
+      view_log(db, view_log_cmd)
     when "3"
-    when "4"
     else
-      user_input = "5"
+      user_input = "4"
     end
 end
-
-
-  # puts "Would you like to make an entry to the garden log? (y or n)"
-  # user_input = gets.chomp
-  # if user_input == "y"
-  #   puts "Enter the date below in the following format"
-  #   puts "year-month-day, (v = vegetables, s = succulents, p = perennials)"
-  #   puts "ex: 2016-07-22, s    for succulents watered on July 22nd 2016"
-  #   user_date = gets.chomp
-  #   water_date = split_date(user_date)[0]
-  #   plant_type = split_date(user_date)[1]
-  #   insert_entry(db, water_date, plant_type)
-  # # p Date._parse(water_date)
-  # else
-  #   user_input = "n"
-  # end
