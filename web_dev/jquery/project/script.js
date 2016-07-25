0749eade9b922606ba2f3806998b27ae0bba2148
Frank Aiello <frankvaiello@gmail.com>
2016-07-25 13:54:01 -0700

    complete challenge 9.2

--- web_dev/jquery/project/script.js
+++ web_dev/jquery/project/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#button').click(function() {
-    var day = document.getElementById("day").value;
+    var day = '#' + document.getElementById("day").value;
     var textarea_input = document.getElementById('textEntryBox').value
-    $('#' + day).append('<div class="item">' + textarea_input + '</div>');
+    $(day).append('<div class="item">' + textarea_input + '</div>');
   });
 });
 
